d08c18630fde80fc7d493bda7c11ba661d17d181
Vicente García Andrade <vicengandrade@gmail.com>
2022-01-29 11:00:21 +0100

    game manager 2.0

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,18 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void ChangeScene(string sc)
     {
-        
+        if (sc == "")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            SceneManager.LoadScene(sc);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+ 
 }
