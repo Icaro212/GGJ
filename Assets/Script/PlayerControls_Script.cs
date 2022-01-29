74928007affff95e99ba792e9f11f9142c6370b8
KanedaXIII <mnemosineii@gmail.com>
2022-01-29 13:24:00 +0100

    ColorSystem

--- Assets/Script/PlayerControls_Script.cs
+++ Assets/Script/PlayerControls_Script.cs
@@ -11,10 +11,21 @@ public class PlayerControls_Script : MonoBehaviour
 
     public Collider2D character;
 
+    public bool colorCheck = false;
+
+    public float colorTime = 2f;
+
+    GameObject[] noColorList;
+
+    GameObject[] colorList;
+
     void Start(){
         rb=GetComponent<Rigidbody2D>();
         character=GetComponent<CapsuleCollider2D>();
-        
+
+
+        noColorList = GameObject.FindGameObjectsWithTag("noColor");
+        colorList = GameObject.FindGameObjectsWithTag("color");
     }
 
     
@@ -24,14 +35,44 @@ public class PlayerControls_Script : MonoBehaviour
             rb.velocity = new Vector2 (horizontal * speed * Time.fixedDeltaTime, rb.velocity.y);
         }
 
-        if(Input.GetButton("Jump") && IsGrounded()){
-            rb.AddForce(new Vector2(rb.velocity.x, (float) 0.8) ,ForceMode2D.Impulse);
-        }
+         
+        
+        //if(Input.GetButton("Jump") && IsGrounded()){
+        //    rb.AddForce(new Vector2(rb.velocity.x, (float) 0.8) ,ForceMode2D.Impulse);
+        //}
         
     }
 
-    bool IsGrounded(){
-        Physics2D.Raycast(character.bounds.center,Vector2.down, character.bounds.extents.y+0.1f);
+    //bool IsGrounded(){
+    //    Physics2D.Raycast(character.bounds.center,Vector2.down, character.bounds.extents.y+0.1f);
+    //}
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            StartCoroutine(takeFairy());
+        }
+    }
+
+    public IEnumerator takeFairy()
+    {
+        changeColor(false);
+        yield return new WaitForSeconds(colorTime);
+        changeColor(true);
+    }
+
+    public void changeColor(bool rBool)
+    {
+        
+        foreach (var i in noColorList)
+        {
+            i.GetComponent<SpriteRenderer>().enabled = rBool;
+        }
+       
+        foreach (var b in colorList)
+        {
+            b.GetComponent<SpriteRenderer>().enabled = !rBool; ;
+        }
     }
 
 
