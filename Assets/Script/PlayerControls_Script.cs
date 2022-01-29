2e52fce21b731ae03ccc2493e8aacdff75c59949
Icaro212 <pablorcmail@gmail.com>
2022-01-29 11:08:44 +0100

    first commit new branch

--- Assets/Script/PlayerControls_Script.cs
+++ Assets/Script/PlayerControls_Script.cs
@@ -7,25 +7,31 @@ public class PlayerControls_Script : MonoBehaviour
     
     public Rigidbody2D rb;
 
-    public float speed= 5f;
+    public float speed;
 
+    public Collider2D character;
 
     void Start(){
         rb=GetComponent<Rigidbody2D>();
+        character=GetComponent<CapsuleCollider2D>();
+        
     }
 
     
     void Update(){
         float horizontal = Input.GetAxis("Horizontal");
+        if(horizontal != 0){
+            rb.velocity = new Vector2 (horizontal * speed * Time.fixedDeltaTime, rb.velocity.y);
+        }
 
-        if(horizontal<0){
-            rb.velocity = new Vector2( rb.velocity.x * horizontal  * speed, rb.velocity.y );
+        if(Input.GetButton("Jump") && IsGrounded()){
+            rb.AddForce(new Vector2(rb.velocity.x, (float) 0.8) ,ForceMode2D.Impulse);
         }
         
-            
-        
-        
+    }
 
+    bool IsGrounded(){
+        Physics2D.Raycast(character.bounds.center,Vector2.down, character.bounds.extents.y+0.1f);
     }
 
 
